6d5e78b1fc48738ca6e2d3a9465beff146ba07d2
Maquiner <jaimerz011235813@gmail.com>
2021-07-06 12:26:58 +0200

    master x for glam

---
+++ scripts/gp_masterx.py
@@ -0,0 +1,309 @@
+import numpy as np
+import pymc3 as pm
+import classy
+import theano
+import theano.tensor as tt
+import os
+import utils
+import make_data
+from scipy.linalg import block_diag
+
+#Load data
+z_max = 1110
+res = 300
+x_arr = np.linspace(0, np.log(1+z_max), res)
+dx = np.mean(np.diff(x_arr))
+z_arr = np.exp(x_arr)-1
+a_arr = 1./(1+z_arr)
+
+path = '/mnt/zfsusers/jaimerz/PhD/Growz/data/products'
+
+tools = utils.utils()
+c = tools.c
+data = make_data.make_data(z_max, res , path)
+
+DESI = data.get_DESI(new=True)
+WFIRST = data.get_WFIRST(new=True)
+CC = data.get_CC(new=True)
+DSS = data.get_DSS(new=True)
+BOSS = data.get_BOSS(new=True)
+eBOSS = data.get_eBOSS(new=True)
+Wigglez = data.get_Wigglez(new=True)
+DS17 = data.get_DS17(new=True)
+CMB = data.get_CMB(new=True)
+FCMB = data.get_FCMB(new=True)
+
+n_samples = 1000
+n_tune = 1000
+datadict = {'DESI': DESI,
+            'WFIRST': WFIRST,
+            'CC': CC,
+            'DS17': DS17, 
+            'BOSS': BOSS,
+            'eBOSS': eBOSS,
+            'Wigglez': Wigglez,
+            'DSS': DSS,
+            'CMB': CMB, 
+            'FCMB': FCMB}
+
+datasets = ['BOSS', 'eBOSS', 'CMB']
+
+need_dM = ['DESI', 'BOSS', 'eBOSS', 'Wigglez', 'DS17', 'CMB']
+need_fs8 = ['DESI', 'BOSS', 'eBOSS', 'Wigglez', 'DSS']
+need_rd = ['BOSS', 'eBOSS', 'CMB']
+
+if any(dataset in datasets for dataset in need_dM):
+    get_dM=True 
+else:
+    get_dM=False
+    
+if any(dataset in datasets for dataset in need_fs8):
+    get_fs8=True
+else:
+    get_fs8=False
+    
+if any(dataset in datasets for dataset in need_rd):
+    get_rd = True
+else:
+    get_rd = False
+        
+#Data
+data = np.array([])
+data_cov = np.array([])
+for dataset_name in datasets:
+    dataset = datadict[dataset_name]
+    data = np.concatenate([data, dataset['data']])
+    data_cov = block_diag(data_cov, dataset['cov'])
+data_cov = data_cov[1:]
+
+#base model
+with pm.Model() as model:
+    #ℓ = pm.InverseGamma("ℓ", alpha=2, beta=z_max/2) 
+    ℓ = pm.InverseGamma("ℓ", alpha=3, beta=0.5) 
+    #ℓ = pm.Uniform("ℓ", 10, 1000) 
+    η = pm.HalfNormal("η", sigma=0.3) 
+    wm0 = 0.138 #pm.Uniform("wm0", 0.1, 0.2) 
+    wL0 = 0.3 #pm.Uniform("wL0", 0.25, 0.35) 
+    wr0 = 2.47*10**-5 + 1.71*10**-5
+    gp_cov = η ** 2 * pm.gp.cov.ExpQuad(1, ℓ) + pm.gp.cov.WhiteNoise(1e-3)
+    gp = pm.gp.Latent(cov_func=gp_cov)
+    
+    #Mean of the gp
+    H = pm.Deterministic('H', 100*tt.sqrt(wm0*(1+z_arr)**3+wr0*(1+z_arr)**4+wL0))
+    
+    #Set up Gaussian process
+    DH_gp = gp.prior("DH_gp", X=x_arr[:, None]) 
+    H_gp = pm.Deterministic("H_gp", tt.as_tensor_variable(H))# *(1+DH_gp)))
+    H0_gp = pm.Deterministic("H0_gp", tt.as_tensor_variable(H_gp[0]))
+    
+    if get_dM:
+        dH_gp = pm.Deterministic("dH", tt.as_tensor_variable((c/1000)*(1+z_arr)/H_gp))
+        dM_rec_gp = tt.zeros(len(z_arr)+1)
+        dM_gp = tt.zeros(len(z_arr))
+        dM_rec_gp = tt.inc_subtensor(dM_rec_gp[1:],
+                  tt.as_tensor_variable(dx*tt.cumsum(dH_gp)))
+        dM_trap_gp = tt.as_tensor_variable(0.5*(dM_rec_gp[1:]+dM_rec_gp[:-1])+0.5*dM_rec_gp[1])
+        dM_gp = pm.Deterministic('dM_gp',
+                tt.inc_subtensor(dM_gp[1:], dM_trap_gp[:-1]))
+        #dM_gp = pm.Deterministic('dM_gp', dM_rec_gp[:-1])
+        dA_gp = pm.Deterministic('dA_gp', dM_gp/(1+z_arr))
+        dL_gp = pm.Deterministic('dL_gp', dM_gp*(1+z_arr))
+        
+    if get_rd:
+        #https://arxiv.org/pdf/2106.00428.pdf
+        wb0 =  0.02226 #pm.Normal("wb0", 0.02226, sigma=0.00023)
+        a1 = 0.00785436
+        a2 = 0.177084
+        a3 = 0.00912388
+        a4 = 0.618711
+        a5 = 11.9611
+        a6 = 2.81343
+        a7 = 0.784719
+        rd_gp = tt.as_tensor_variable(1/(a1*wb0**a2+a3*wm0**a4+a5*wb0**a6*wm0**a7))  
+        
+    if get_fs8:
+        s80 = pm.Normal("s80", 0.8, 0.5)
+        Wm0 =  pm.Deterministic('Wm0', wm0*(100/H_gp[0])**2)
+        E_gp = pm.Deterministic('E_gp', H_gp/H_gp[0])
+        
+        d = tt.zeros(len(z_arr))
+        y = tt.zeros(len(z_arr))
+        d = tt.inc_subtensor(d[-1], a_arr[-1])
+        y = tt.inc_subtensor(y[-1], E_gp[0]*a_arr[-1]**3)
+        for i in np.arange(1, len(z_arr)):
+            A0 = -1.5*Wm0/(a_arr[-i]*E_gp[-i])
+            B0 = -1./(a_arr[-i]**2*E_gp[-i])
+            A1 = -1.5*Wm0/(a_arr[-(i+1)]*E_gp[-(i+1)])
+            B1 = -1./(a_arr[-(i+1)]**2*E_gp[-(i+1)])
+            y = tt.inc_subtensor(y[-(i+1)], (1-0.5*dx**2*A0*B0)*y[-i]-0.5*(A0+A1)*dx*d[-i])
+            d = tt.inc_subtensor(d[-(i+1)], -0.5*(B0+B1)*dx*y[-i]+(1-0.5*dx**2*A0*B0)*d[-i])
+        
+        fs8_gp = pm.Deterministic('fs8_gp', s80*y/(a_arr**2*E_gp*d[0]))
+        s8_gp = pm.Deterministic('s8_gp', s80*d/d[0])
+    
+    theory = tt.as_tensor_variable([])
+    
+#Modules
+if 'DESI' in datasets:
+    print('Adding DESI')
+    with model:
+        DESI_H = pm.Deterministic('DESI_H',
+                 tt.as_tensor_variable(H_gp[DESI['idx']]+(H_gp[DESI['idx']+1]-H_gp[DESI['idx']])*DESI['U']))
+        DESI_dA = pm.Deterministic('DESI_dA',
+                  tt.as_tensor_variable(dA_gp[DESI['idx']]+(dA_gp[DESI['idx']+1]-dA_gp[DESI['idx']])*DESI['U']))
+        DESI_fs8 = pm.Deterministic('DESI_fs8',
+                   tt.as_tensor_variable(fs8_gp[DESI['idx']]+(fs8_gp[DESI['idx']+1]-fs8_gp[DESI['idx']])*DESI['U']))
+        theory = tt.concatenate([theory, DESI_H, DESI_dA, DESI_fs8])
+        
+if 'WFIRST' in datasets:
+    print('Adding WFIRST')
+    with model:
+        WFIRST_E = pm.Deterministic('WFIRST_E',
+                   tt.as_tensor_variable(E_gp[WFIRST['idx']]+(E_gp[WFIRST['idx']+1]-E_gp[WFIRST['idx']])*WFIRST['U']))
+        theory = tt.concatenate([theory, WFIRST_E])
+
+if 'CC' in datasets:
+    print('Adding CCs')
+    with model:
+        CC_H = pm.Deterministic("CC_H",
+               tt.as_tensor_variable(H_gp[CC['idx']]+(H_gp[CC['idx']+1]-H_gp[CC['idx']])*CC['U']))
+        theory = tt.concatenate([theory, CC_H])
+        
+if 'DS17' in datasets:
+    print('Adding Pantheon')
+    with model:
+        M = pm.Normal('M', mu=-19.0, sigma=3)
+        u_gp = pm.Deterministic('u_gp', tt.as_tensor_variable(5*tt.log10(dL_gp)+25+M))
+        DS17_u = pm.Deterministic("DS17_u",
+                 tt.as_tensor_variable(u_gp[DS17['idx']]+(u_gp[DS17['idx']+1]-u_gp[DS17['idx']])*DS17['U']))
+        theory = tt.concatenate([theory, DS17_u])
+        
+if 'BOSS' in datasets:
+    print('Adding BOSS')
+    with model:
+        #Get alpha_perp and alpha_para 
+        B_para_f = pm.Deterministic("B_para_f", H_gp*rd_gp/BOSS['rd'])
+        B_perp_f = pm.Deterministic("B_perp_f", dM_gp*BOSS['rd']/rd_gp)
+        
+        B_para = pm.Deterministic("B_para", 
+                    tt.as_tensor_variable(B_para_f[BOSS['idx']]+(B_para_f[BOSS['idx']+1]-B_para_f[BOSS['idx']])*BOSS['U']))
+        B_perp = pm.Deterministic("B_perp", 
+                tt.as_tensor_variable(B_perp_f[BOSS['idx']]+(B_perp_f[BOSS['idx']+1]-B_perp_f[BOSS['idx']])*BOSS['U']))
+        B_fs8 = pm.Deterministic("B_fs8", 
+                   tt.as_tensor_variable(fs8_gp[BOSS['idx']]+(fs8_gp[BOSS['idx']+1]-fs8_gp[BOSS['idx']])*BOSS['U']))
+        
+        theory = tt.concatenate([theory, B_para, B_perp, B_fs8])
+        
+if 'eBOSS' in datasets:
+    print('Adding eBOSS')
+    with model:
+        eB_para_f = pm.Deterministic("eB_para_f", dH_gp/rd_gp)
+        eB_perp_f = pm.Deterministic("eB_perp_f", dH_gp/rd_gp)
+        
+        eB_para = pm.Deterministic("eB_para", 
+                    tt.as_tensor_variable(eB_para_f[eBOSS['idx']]+(eB_para_f[eBOSS['idx']+1]-eB_para_f[eBOSS['idx']])*eBOSS['U']))
+        eB_perp = pm.Deterministic("eB_perp", 
+                tt.as_tensor_variable(eB_perp_f[eBOSS['idx']]+(eB_perp_f[eBOSS['idx']+1]-eB_perp_f[eBOSS['idx']])*eBOSS['U']))
+        eB_fs8 = pm.Deterministic("eB_fs8", 
+                   tt.as_tensor_variable(fs8_gp[eBOSS['idx']]+(fs8_gp[eBOSS['idx']+1]-fs8_gp[eBOSS['idx']])*eBOSS['U']))
+        
+        theory = tt.concatenate([theory, eB_para, eB_perp, eB_fs8])
+
+if 'Wigglez' in datasets:
+    print('Adding Wigglez')
+    with model:
+        Wigglez_fs8 = pm.Deterministic("Wigglez_fs8",
+                    tt.as_tensor_variable(fs8_gp[Wigglez['idx']]+(fs8_gp[Wigglez['idx']+1]-fs8_gp[Wigglez['idx']])*Wigglez['U']))
+        theory = tt.concatenate([theory, Wigglez_fs8])
+
+if 'DSS' in datasets:
+    print('Adding DSS')
+    with model:
+        DSS_fs8 = pm.Deterministic("fs8_eBOSS", tt.as_tensor_variable(fs8_gp[DSS['idx']]))
+        theory = tt.concatenate([theory, DSS_fs8])
+
+if 'CMB' in datasets:
+    print('Adding CMB')
+    with model:
+        dM_star = tt.as_tensor_variable(dM_gp[CMB['idx']]+(dM_gp[CMB['idx']+1]-dM_gp[CMB['idx']])*CMB['U'])
+        t100 = pm.Deterministic('t100', 100*rd_gp/dM_star) 
+        theory = tt.concatenate([theory, t100])
+        
+if 'FCMB' in datasets:
+    print('Adding FCMB')
+    with model:
+        CMB_H = pm.Deterministic('CMB_H',
+                  tt.as_tensor_variable(H_gp[FCMB['idx']]+(H_gp[FCMB['idx']+1]-H_gp[FCMB['idx']])*FCMB['U']))
+        theory = tt.concatenate([theory, CMB_H])
+        
+#sampling
+with model:
+    lkl= pm.MvNormal("lkl", mu=theory, cov=data_cov, observed=data)
+    trace = pm.sample(n_samples, return_inferencedata=True, tune=n_tune)
+
+    
+#print r-stat
+print(pm.summary(trace)['r_hat'][["ℓ","η"]])
+print(pm.summary(trace)['mean'][["ℓ","η"]])
+
+#Save
+filename = ''
+for dataset in datasets:
+    filename+=dataset+'_'
+path = filename+'{}_{}_t'.format(n_samples, n_tune)
+
+n = np.array(trace.posterior["η"]).flatten()
+l = np.array(trace.posterior["ℓ"]).flatten()
+DHz = np.array(trace.posterior["DH_gp"])
+DHz = DHz.reshape(-1, DHz.shape[-1])
+Hz =np.array(trace.posterior["H_gp"])
+Hz = Hz.reshape(-1, Hz.shape[-1])
+H0 = np.array(trace.posterior["H0_gp"]).flatten()
+omega_m = np.array(trace.posterior["wm0"]).flatten()
+omega_L = np.array(trace.posterior["wl0"]).flatten()
+
+if get_dM:
+    dMz = np.array(trace.posterior["dM_gp"])
+    dMz = dMz.reshape(-1, dMz.shape[-1])
+else:
+    dMz = None
+
+if get_rd:
+    omega_b = np.array(trace.posterior["wb0"]).flatten()
+else:
+    omega_b = None
+    
+if get_fs8:
+    s8z = np.array(trace.posterior["s8_gp"])
+    s8z = s8z.reshape(-1, s8z.shape[-1])
+    fs8z = np.array(trace.posterior["fs8_gp"])
+    fs8z = fs8z.reshape(-1, fs8z.shape[-1])
+    s80 = np.array(trace.posterior["s80"]).flatten()
+    S80 = s80*np.sqrt(Omega_m/0.3)
+else: 
+    s8z = None 
+    fs8z = None
+    s80 = None
+    S80 = None
+
+if 'DS17' in datasets:
+    M = np.array(trace.posterior["M"]).flatten()
+else:
+    M = None
+
+os.mkdir(path)
+np.savez(os.path.join(path,'samples.npz'), 
+         z_arr = z_arr,
+         n=n,
+         l=l,
+         DHz = DHz,
+         Hz=Hz,
+         dMz=dMz,
+         s8z=s8z,
+         fs8z=fs8z,
+         H0=H0,
+         omega_m=omega_m,
+         omega_b=omega_b,
+         omega_L=omega_L,
+         s80=s80,
+         S80=S80)
---
+++ scripts/make_data_old.py
@@ -0,0 +1,353 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+import utils_old
+from pandas import read_table
+class make_data():
+    def __init__(self, z_max, res, path):
+        self.path = path
+        self.tools = utils.utils()
+        self.res = res
+        self.dz = 10**(-res)
+        self.z_max = z_max
+        self.z_arr = np.arange(0.0, self.z_max+0.1, self.dz)
+        self.a_arr = 1/(1+self.z_arr) 
+        self.x_arr = np.log(self.a_arr)
+        self.Planck = self.tools.get_preds(self.z_arr, mode = 'Planck')
+    
+    def get_WFIRST(self, new='False'):
+        dataset_name = 'WFIRST'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            WFIRST_rels_E  = np.array([1.3, 1.1, 1.5, 
+                                1.5, 2.0, 2.3, 
+                                2.6, 3.4, 8.9])
+            z_WFIRST = np.array([0.07, 0.2, 0.35, 
+                              0.6, 0.8, 1.0,
+                              1.3, 1.7, 2.5])
+            WFIRST_idx = np.array([int(x) for x in z_WFIRST/(self.dz)])
+            E_arr = self.Planck['Hkms_arr']/self.Planck['Hkms_arr'][0]
+            WFIRST_err = E_arr[WFIRST_idx]*WFIRST_rels_E/100
+            WFIRST_data = E_arr[WFIRST_idx]+ np.random.randn(len(z_WFIRST))*WFIRST_err
+            WFIRST_cov = np.zeros([len(z_WFIRST), len(z_WFIRST)])
+            for i in np.arange(len(z_WFIRST)):
+                WFIRST_cov[i,i] = WFIRST_err[i]**2
+                
+            np.savez(os.path.join(self.path, dataset_name), 
+                     data = WFIRST_data,
+                     z=z_WFIRST,
+                     cov=WFIRST_cov,
+                     err=WFIRST_err, 
+                     idx=WFIRST_idx )
+            
+        return np.load(filepath)
+        
+    def get_DESI(self, new='False'):
+        dataset_name = 'DESI'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            DESI_rels_dA  = [2.78, 1.87, 1.45, 1.19, 1.01, 0.87, 0.77, 
+             0.76, 0.88, 0.91, 0.91, 0.91, 1.00, 1.17, 
+             1.50, 2.36, 3.62, 4.79]
+            DESI_rels_H = [5.34, 3.51, 2.69, 2.20, 1.85, 1.60, 1.41, 
+                     1.35, 1.42, 1.41, 1.38, 1.36, 1.46, 1.66, 
+                     2.04, 3.15, 4.87, 6.55]
+            DESI_rels_fs8 = [3.60, 2.55, 2.17, 1.91, 1.60, 1.24, 
+                     1.18, 1.11, 1.13, 1.12, 1.12, 1.14, 1.26, 
+                     1.47, 1.89, 3.06, 5.14, 7.66]
+
+            z_DESI = np.arange(0.15, 1.85+0.1, 0.1)
+            DESI_idx = np.array([int(x) for x in z_DESI/(self.dz)])
+
+            H_arr_f = self.Planck['Hkms_arr']
+            dA_arr_f = self.tools.make_dM((1000/self.tools.c)*H_arr_f, self.z_arr)
+            dA_arr_f[1:] /= (1+self.z_arr)[1:]
+            f_arr_f = self.tools.make_f(H_arr_f, self.z_arr, 0.31, 0.44)
+            s8_arr_f = self.tools.make_sigma8(f_arr_f, self.z_arr, 0.805)
+            fs8_arr = self.Planck['f_arr']*self.Planck['s8_arr']
+
+
+            DESI_H_err = H_arr_f[DESI_idx]*DESI_rels_H/100
+            DESI_dA_err = dA_arr_f[DESI_idx]*DESI_rels_dA/100
+            DESI_fs8_err = fs8_arr[DESI_idx]*DESI_rels_fs8/100
+
+            DESI_err = np.concatenate([DESI_H_err, DESI_dA_err, DESI_fs8_err])
+
+            DESI_H_data = H_arr_f[DESI_idx] + np.random.randn(len(z_DESI))*DESI_H_err
+            DESI_dA_data = dA_arr_f[DESI_idx] + np.random.randn(len(z_DESI))*DESI_dA_err
+            DESI_fs8_data = fs8_arr[DESI_idx] + np.random.randn(len(z_DESI))*DESI_fs8_err
+            DESI_data = np.concatenate([DESI_H_data, DESI_dA_data, DESI_fs8_data])
+
+            DESI_H_cov = np.zeros([len(z_DESI), len(z_DESI)])
+            DESI_dA_cov = np.zeros([len(z_DESI), len(z_DESI)])
+            DESI_fs8_cov = np.zeros([len(z_DESI), len(z_DESI)])
+            for i in np.arange(len(z_DESI)):
+                DESI_H_cov[i,i] = DESI_H_err[i]**2
+                DESI_dA_cov[i,i] = DESI_dA_err[i]**2
+                DESI_fs8_cov[i,i] = DESI_fs8_err[i]**2
+
+            DESI_cov = np.block([[DESI_H_cov, np.zeros_like(DESI_H_cov), np.zeros_like(DESI_H_cov)],
+                             [np.zeros_like(DESI_H_cov), DESI_dA_cov, np.zeros_like(DESI_H_cov)],
+                             [np.zeros_like(DESI_H_cov), np.zeros_like(DESI_H_cov), DESI_fs8_cov]])
+
+            np.savez(os.path.join(self.path, dataset_name), 
+             data = DESI_data,
+             z=z_DESI,
+             cov=DESI_cov,
+             err=DESI_err, 
+             H_data = DESI_H_data, 
+             dA_data = DESI_dA_data,
+             fs8_data = DESI_fs8_data,
+             H_err = DESI_H_err, 
+             dA_err = DESI_dA_err,
+             fs8_err = DESI_fs8_err, 
+             idx = DESI_idx)
+        
+        return np.load(filepath)
+    
+    def get_DS17(self, new='False'):
+        dataset_name = 'DS17'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            SN = self.tools.read_light_curve_parameters('/mnt/zfsusers/jaimerz/PhD/Growz/data/raw/PantheonDS17/lcparam_DS17f.txt')
+            SN_data = np.array(SN.mb)
+            z_SN = np.array(SN.zcmb)
+            SN_idx = np.array([int(x) for x in z_SN/(self.dz)])
+            SN_cov = np.genfromtxt('/mnt/zfsusers/jaimerz/PhD/Growz/data/raw/PantheonDS17/syscov_panth.txt') + np.diag(SN.dmb**2)
+            SN_err = np.sqrt(np.diag(SN_cov))
+
+            np.savez(os.path.join(self.path, dataset_name),  
+                     data = SN_data,
+                     z=z_SN,
+                     cov=SN_cov,
+                     err=SN_err, 
+                     idx = SN_idx)
+        
+        return np.load(filepath)
+    
+    def get_CC(self, new='False'):
+        dataset_name = 'CC'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            return np.load(filepath)
+        else:
+            CC_data = np.array([69. ,  69. ,  68.6,  83. ,  75. ,  75. ,  72.9,  77. ,
+                    88.8,  83. ,  81.5,  83. ,  95. ,  77. ,  87.1,  82.6,  92.8,
+                    89. ,  80.9,  97. ,  90.4, 104. ,  87.9,  97.3,  92. ,  97.3,
+                   105. , 125. ,  90. , 117. , 154. , 168. , 160. , 177. , 140. ,
+                   202. , 186.5, 222. , 226. ])
+            z_CC = np.array([0.07  , 0.09  , 0.12  , 0.17  , 0.179 , 0.199 , 0.2   ,
+                   0.27  , 0.28  , 0.352 , 0.38  , 0.3802, 0.4   , 0.4004, 0.4247,
+                   0.44  , 0.4497, 0.47  , 0.4783, 0.48  , 0.51  , 0.593 , 0.6   ,
+                   0.61  , 0.68  , 0.73  , 0.781 , 0.875 , 0.88  , 0.9   , 1.037 ,
+                   1.3   , 1.363 , 1.43  , 1.53  , 1.75  , 1.965 , 2.34  , 2.36  ])
+            CC_err= np.array([19.6,  12. ,  26.2,   8. ,   4. ,   5. ,  29.6,  14. ,
+                    36.6,  14. ,   1.9,  13.5,  17. ,  10.2,  11.2,   7.8,  12.9,
+                    23. ,   9. ,  62. ,   1.9,  13. ,   6.1,   2.1,   8. ,   7. ,
+                    12. ,  17. ,  40. ,  23. ,  20. ,  17. ,  33.6,  18. ,  14. ,
+                    40. ,  50.4,   7. ,   8. ])
+            CC_idx = np.array([int(x) for x in z_CC/(self.dz)])
+            CC_cov = np.zeros([len(z_CC),len(z_CC)])
+            for i in np.arange(len(z_CC)):
+                CC_cov[i,i] = CC_err[i]**2
+            np.savez(os.path.join(self.path, dataset_name),  
+                     data = CC_data,
+                     z=z_CC,
+                     cov=CC_cov,
+                     err=CC_err, 
+                     idx=CC_idx)
+        
+        return np.load(filepath)
+    
+    def get_Wigglez(self, new='False'):
+        dataset_name = 'Wigglez'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_Wigglez = np.array([0.44, 0.60, 0.73])
+            fs8_Wigglez = np.array([0.413, 0.390, 0.437])
+            data_Wigglez = np.concatenate([fs8_Wigglez])
+            Wigglez_idx = np.array([int(x) for x in z_Wigglez/(self.dz)])
+            Wigglez_cov = 10**(-3)*np.array([[6.4, 2.57, 0], 
+                                            [2.57, 3.969, 2.54], 
+                                            [0, 2.54, 5.184]])
+            Wigglez_fs8_err = np.sqrt(np.diag(Wigglez_cov))
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = fs8_Wigglez,
+             z=z_Wigglez,
+             cov=Wigglez_cov,
+             err=Wigglez_fs8_err, 
+             idx=Wigglez_idx)
+        
+        return np.load(filepath)
+    
+    def get_DSS(self, new='False'):
+        dataset_name = 'DSS'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_DSS = np.array([0])
+            fs8_DSS = np.array([0.39])
+            DSS_cov = np.array([[0.022**2]])
+            DSS_err = np.array([0.022])
+            DSS_idx = [0]
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = fs8_DSS,
+             z=z_DSS,
+             cov=DSS_cov,
+             err=DSS_err, 
+             idx=DSS_idx)
+        
+        return np.load(filepath)
+    
+    def get_CMB(self, new='False'):
+        dataset_name = 'CMB'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_CMB = np.array([1090.30])
+            perp_CMB = np.array([1.04097])
+            CMB_cov = np.array([[0.00046**2]])
+            CMB_err = np.array([0.00046])
+            CMB_rd = 144.46 #+- 0.48
+            CMB_idx = [-2]
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = perp_CMB,
+             rd=CMB_rd,
+             z=z_CMB,
+             cov=CMB_cov,
+             err=CMB_err, 
+             idx=CMB_idx)
+        
+        return np.load(filepath)
+    
+    def get_FCMB(self, new='False'):
+        dataset_name = 'CMB'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_CMB = np.array([1080.30])
+            para_CMB = np.array([1541677.34])
+            CMB_cov = np.array([[1541.67**2]])
+            CMB_err = np.array([1541.67])
+            CMB_idx = np.array([int(x) for x in np.array([1080.3])/(0.1)])
+            CMB_idx += 251-25
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = para_CMB,
+             z=z_CMB,
+             cov=CMB_cov,
+             err=CMB_err, 
+             idx=CMB_idx)
+        
+        return np.load(filepath)
+    
+    def get_eBOSS(self, new='False'):
+        dataset_name = 'eBOSS'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_eBOSS = np.array([1.48])
+            para_eBOSS = 13.23
+            perp_eBOSS = 30.21
+            fs8_eBOSS = 0.462
+            rd_eBOSS = 147.3 #double check
+            eBOSS_idx = np.array([int(x) for x in z_eBOSS/(self.dz)])
+            data_eBOSS = np.array([13.11, 30.66, 0.439])
+            eBOSS_cov = np.array([[0.7709, -0.05656, 0.01750],
+                                  [-0.05656, 0.2640, -0.006204],
+                                  [0.01750, -0.006204, 0.002308]])
+            eBOSS_H_err = np.sqrt(np.diag(eBOSS_cov))[0]
+            eBOSS_dA_err = np.sqrt(np.diag(eBOSS_cov))[1]
+            eBOSS_fs8_err = np.sqrt(np.diag(eBOSS_cov))[2]
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = data_eBOSS,
+             z=z_eBOSS,
+             cov=eBOSS_cov,
+             para_data=para_eBOSS,
+             perp_data=perp_eBOSS,
+             fs8_data=fs8_eBOSS,
+             para_err=eBOSS_H_err,
+             perp_err=eBOSS_dA_err,
+             fs8_err=eBOSS_fs8_err, 
+             rd=rd_eBOSS, 
+             idx=eBOSS_idx)
+        
+        return np.load(filepath)
+    
+    def get_BOSS(self, new='False'):
+        dataset_name = 'BOSS'
+        filepath = os.path.join(self.path, dataset_name+'.npz')
+        if (os.path.exists(filepath)) and (new is False):
+            print('Found file for '+ dataset_name)
+            pass
+        else:
+            z_BOSS = np.array([0.38, 0.51, 0.61])
+            data_BOSS = np.array([1512.39, 81.2087, 0.49749,
+                             1975.22, 90.9029, 0.457523,
+                             2306.68, 98.9647,  0.436148])
+            perp_BOSS = np.array([1512.39, 1975.22, 2306.68])
+            para_BOSS = np.array([81.2087, 90.9029, 98.9647])
+            fs8_BOSS = np.array([0.49749, 0.457523, 0.436148])
+            data_BOSS = np.concatenate([para_BOSS, perp_BOSS, fs8_BOSS])
+            BOSS_idx = np.array([int(x) for x in z_BOSS/(self.dz)])
+            BOSS_cov = np.array([[3.63049e+00, 1.80306e+00, 9.19842e-01, 9.71342e+00, 7.75546e+00,
+                    5.97897e+00, 2.79185e-02, 1.24050e-02, 4.75548e-03],
+                   [1.80306e+00, 3.77146e+00, 2.21471e+00, 4.85105e+00, 1.19729e+01,
+                    9.73184e+00, 9.28354e-03, 2.22588e-02, 1.05956e-02],
+                   [9.19842e-01, 2.21471e+00, 4.37982e+00, 2.43394e+00, 6.71715e+00,
+                    1.60709e+01, 1.01870e-03, 9.71991e-03, 2.14133e-02],
+                   [9.71342e+00, 4.85105e+00, 2.43394e+00, 5.00049e+02, 2.94536e+02,
+                    1.42011e+02, 3.91498e-01, 1.51597e-01, 4.36366e-02],
+                   [7.75546e+00, 1.19729e+01, 6.71715e+00, 2.94536e+02, 7.02299e+02,
+                    4.32750e+02, 1.95890e-01, 3.88996e-01, 1.81786e-01],
+                   [5.97897e+00, 9.73184e+00, 1.60709e+01, 1.42011e+02, 4.32750e+02,
+                    1.01718e+03, 3.40803e-02, 2.46111e-01, 4.78570e-01],
+                   [2.79185e-02, 9.28354e-03, 1.01870e-03, 3.91498e-01, 1.95890e-01,
+                    3.40803e-02, 2.03355e-03, 8.11829e-04, 2.64615e-04],
+                   [1.24050e-02, 2.22588e-02, 9.71991e-03, 1.51597e-01, 3.88996e-01,
+                    2.46111e-01, 8.11829e-04, 1.42289e-03, 6.62824e-04],
+                   [4.75548e-03, 1.05956e-02, 2.14133e-02, 4.36366e-02, 1.81786e-01,
+                    4.78570e-01, 2.64615e-04, 6.62824e-04, 1.18576e-03]])
+
+            BOSS_err = np.sqrt(np.diag(BOSS_cov))
+            BOSS_para_err = np.array([BOSS_err[0], BOSS_err[1], BOSS_err[2]])
+            BOSS_perp_err = np.array([BOSS_err[3], BOSS_err[4], BOSS_err[5]])
+            BOSS_fs8_err = np.array([BOSS_err[6], BOSS_err[7], BOSS_err[8]])
+
+            rd_BOSS = 147.78
+            z_BOSS_f = np.arange(0.0, 0.61+0.1, self.dz)
+            np.savez(os.path.join(self.path, dataset_name),  
+             data = data_BOSS,
+             z=z_BOSS,
+             cov=BOSS_cov,
+             para_data=para_BOSS,
+             perp_data=perp_BOSS,
+             fs8_data=fs8_BOSS,
+             para_err=BOSS_para_err,
+             perp_err=BOSS_perp_err,
+             fs8_err=BOSS_fs8_err,
+             rd=rd_BOSS, 
+             idx=BOSS_idx)
+        
+        return np.load(filepath)
