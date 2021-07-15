fixed inconsistencies and fs8 formula

notebooks/make_data.py
@@ -4,11 +4,12 @@ import pandas as pd
 import os
 import utils
 from pandas import read_table
+import pyccl as ccl
 
 class make_data():
     def __init__(self, z_max, res, path):
+        self.c = 299792458.0
         self.path = path
-        self.tools = utils.utils()
         self.res = res
         self.z_max = z_max
         self.x_arr = np.linspace(0, np.log(1+self.z_max), self.res)
@@ -16,12 +17,53 @@ class make_data():
         self.a_arr = 1./(1+self.z_arr) 
         self.dx = np.mean(np.diff(self.x_arr))
         self.dz = np.diff(self.z_arr)
+        self.cosmo = self.get_cosmo(mode='Planck')
+            
         if self.z_max > 1085:
             self.z_planck = self.z_arr[self.z_arr<1085]
         else:
             self.z_planck = self.z_arr
-        self.Planck = self.tools.get_preds(self.z_arr[self.z_arr<1085], mode='Planck')
+        self.Planck = utils.get_preds(self.z_arr[self.z_arr<1085], self.cosmo)
+        
+        bg = self.cosmo.get_background()
+        self.Omega_nu = bg['(.)rho_ur'][-1]/(bg['H [1/Mpc]'][-1])**2
+        self.wm0 = self.cosmo.Omega_m() * self.cosmo.h()**2
+        self.wb0 = self.cosmo.Omega_b() * self.cosmo.h()**2
+        self.wr0 = (self.cosmo.Omega_g()+self.Omega_nu) * self.cosmo.h()**2
+        self.wL0 = self.cosmo.Omega_Lambda() * self.cosmo.h()**2
+        
+        self.H_arr = 100*np.sqrt(self.wm0*(1+self.z_arr)**3+(self.wr0)*(1+self.z_arr)**4+self.wL0)
+        self.dM_arr = utils.make_dM((1000/self.c)*self.H_arr, self.x_arr)
+        self.dA_arr = self.dM_arr/(1+self.z_arr)
+        self.s8_arr, self.fs8_arr = utils.make_fs8(self.H_arr, self.x_arr,
+                                                   self.wm0, self.cosmo.sigma8())
 
+    def get_cosmo(self, mode):
+        if mode == 'Planck':
+            params = {'h': 0.6727,
+              'Omega_cdm': 0.265,
+              'Omega_b': 0.050,
+              'n_s': 0.965,
+              'ln10^{10}A_s': 3.045}
+        if mode == 'Riess':
+            params = {'h': 0.74,
+              'Omega_cdm': 0.21,
+              'Omega_b': 0.050,
+              'n_s': 0.9665,
+              'ln10^{10}A_s': 3.040}
+        if mode == 'Panth':
+            params = {'h': 0.724,
+              'Omega_cdm': 0.262,
+              'Omega_b': 0.037,
+              'n_s': 0.837,
+              'ln10^{10}A_s': 3.138}
+        cosmo = ccl.boltzmann.classy.Class()
+        cosmo.set({ 'output':'mPk', 'P_k_max_h/Mpc': 20, 'z_max_pk': 1085})
+        cosmo.set(params)
+        cosmo.compute()
+        self.cosmo = cosmo
+        return self.cosmo
+        
     def make_idx(self, z_data, z_arr):
         idxs = np.array([])
         for z in z_data:
@@ -53,7 +95,7 @@ class make_data():
                               1.3, 1.7, 2.5])
             WFIRST_idx =  self.make_idx(z_WFIRST, z_arr) 
             WFIRST_U = self.make_U(z_WFIRST, z_arr, WFIRST_idx)
-            E_arr = self.Planck['Hkms_arr']/self.Planck['Hkms_arr'][0]
+            E_arr = self.H_arr/self.H_arr[0]
             WFIRST_err = E_arr[WFIRST_idx]*WFIRST_rels_E/100
             WFIRST_data = E_arr[WFIRST_idx]+ np.random.randn(len(z_WFIRST))*WFIRST_err
             WFIRST_cov = np.zeros([len(z_WFIRST), len(z_WFIRST)])
@@ -96,18 +138,10 @@ class make_data():
             DESI_idx =  self.make_idx(z_DESI, z_arr)
             DESI_U = self.make_U(z_DESI, z_arr, DESI_idx)
 
-            H_arr = self.Planck['Hkms_arr']
-            x_arr = self.x_arr[self.z_arr<1085]
-            z_arr = self.z_arr[self.z_arr<1085]
-            dA_arr = self.tools.make_dM((1000/self.tools.c)*H_arr, x_arr)
-            dA_arr /= (1+z_arr)
-            s8_arr, fs8_arr = self.tools.make_fs8(H_arr, x_arr, 0.1422, 0.812, mode='david')
-            
-            
-            H_DESI = H_arr[DESI_idx]+(H_arr[DESI_idx+1]-H_arr[DESI_idx])*DESI_U
-            dA_DESI = dA_arr[DESI_idx]+(dA_arr[DESI_idx+1]-dA_arr[DESI_idx])*DESI_U
-            s8_DESI = s8_arr[DESI_idx]+(s8_arr[DESI_idx+1]-s8_arr[DESI_idx])*DESI_U
-            fs8_DESI = fs8_arr[DESI_idx]+(fs8_arr[DESI_idx+1]-fs8_arr[DESI_idx])*DESI_U
+            H_DESI = self.H_arr[DESI_idx]+(self.H_arr[DESI_idx+1]-self.H_arr[DESI_idx])*DESI_U
+            dA_DESI = self.dA_arr[DESI_idx]+(self.dA_arr[DESI_idx+1]-self.dA_arr[DESI_idx])*DESI_U
+            s8_DESI = self.s8_arr[DESI_idx]+(self.s8_arr[DESI_idx+1]-self.s8_arr[DESI_idx])*DESI_U
+            fs8_DESI = self.fs8_arr[DESI_idx]+(self.fs8_arr[DESI_idx+1]-self.fs8_arr[DESI_idx])*DESI_U
             
             DESI_H_err = H_DESI*DESI_rels_H/100
             DESI_dA_err = dA_DESI*DESI_rels_dA/100
@@ -180,7 +214,7 @@ class make_data():
             print('Found file for '+ dataset_name)
             pass
         else:
-            SN = self.tools.read_light_curve_parameters('/home/jaimerz/PhD/Growz/data/raw/PantheonDS17/lcparam_DS17f.txt')
+            SN = utils.read_light_curve_parameters('/home/jaimerz/PhD/Growz/data/raw/PantheonDS17/lcparam_DS17f.txt')
             SN_data = np.array(SN.mb)
             z_SN = np.array(SN.zcmb)
             SN_idx =  self.make_idx(z_SN, z_arr) 
@@ -299,16 +333,15 @@ class make_data():
             print('Found file for '+ dataset_name)
             pass
         else:
-            z_CMB = np.array([1090.30])
-            CMB_rd = 144.46 
+            z_CMB = np.array([1090.30]) 
+            CMB_rd = utils.make_rd(self.wm0, self.wb0) 
             CMB_idx =  self.make_idx(z_CMB, z_arr)
             CMB_U = self.make_U(z_CMB, z_arr, CMB_idx)
             
-            H_arr = 100*np.sqrt(0.1422*(1+z_arr)**3+((2.47+1.71)*10**-5)*(1+z_arr)**4+0.30)
-            dM_arr = self.tools.make_dM((1000/self.tools.c)*H_arr, self.x_arr)
-            dM_CMB = dM_arr[CMB_idx]+(dM_arr[CMB_idx+1]-dM_arr[CMB_idx])*CMB_U
+            dM_CMB = self.dM_arr[CMB_idx]+(self.dM_arr[CMB_idx+1]-self.dM_arr[CMB_idx])*CMB_U
             perp_CMB = 100*(CMB_rd/dM_CMB)
             
+            #CMB_rd = 144.46
             #perp_CMB = np.array([1.04097])
             
             CMB_cov = np.array([[0.00046**2]])
@@ -336,10 +369,7 @@ class make_data():
             z_FCMB = np.array([1090.30])
             FCMB_idx =  self.make_idx(z_FCMB, z_arr)
             FCMB_U = self.make_U(z_FCMB, z_arr, FCMB_idx)
-            
-            H_arr = 100*np.sqrt(0.1422*(1+z_arr)**3+((2.47+1.71)*10**-5)*(1+z_arr)**4+0.30)
-            dM_arr = self.tools.make_dM((1000/self.tools.c)*H_arr, self.x_arr)
-            dM_CMB = dM_arr[FCMB_idx]+(dM_arr[FCMB_idx+1]-dM_arr[FCMB_idx])*FCMB_U
+            dM_CMB = self.dM_arr[FCMB_idx]+(self.dM_arr[FCMB_idx+1]-self.dM_arr[FCMB_idx])*FCMB_U
             
             #perp_CMB = np.array([1.04097])
             

scripts/gp_masterx.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pymc3 as pm
+import scipy as sp
 import classy
 import theano
 import theano.tensor as tt
@@ -18,9 +20,10 @@ a_arr = 1./(1+z_arr)
 
 path = '/mnt/zfsusers/jaimerz/PhD/Growz/data/products'
 
-tools = utils.utils()
-c = tools.c
 data = make_data.make_data(z_max, res , path)
+Planck = data.Planck
+z_planck = data.z_planck
+c = data.c
 
 DESI = data.get_DESI(new=True, mode=None)
 H_DESI = data.get_DESI(new=True, mode='H')
@@ -37,7 +40,7 @@ CMB = data.get_CMB(new=True)
 FCMB = data.get_FCMB(new=True)
 
 n_samples = 1000
-n_tune = 1000
+n_tune = 2500
 datadict = {'DESI': DESI,
             'H_DESI': H_DESI,
             'dA_DESI': dA_DESI,
@@ -95,7 +98,7 @@ with pm.Model() as model:
     #wL0 = 0.307
     wm0 = pm.Uniform("wm0", 0., 0.45) 
     wL0 = pm.Uniform("wL0", 0., 0.45) 
-    wr0 = (2.47+1.71)*10**-5
+    wr0 = 4.183709411969532e-05
     gp_cov = η ** 2 * pm.gp.cov.ExpQuad(1, ℓ) + pm.gp.cov.WhiteNoise(1e-3)
     gp = pm.gp.Latent(cov_func=gp_cov)
     
@@ -104,7 +107,7 @@ with pm.Model() as model:
     
     #Set up Gaussian process
     DH_gp = gp.prior("DH_gp", X=x_arr[:, None]) 
-    H_gp = pm.Deterministic("H_gp", tt.as_tensor_variable(H*(1+DH_gp)))
+    H_gp = pm.Deterministic("H_gp", tt.as_tensor_variable(H)) #*(1+DH_gp)))
     H0_gp = pm.Deterministic("H0_gp", tt.as_tensor_variable(H_gp[0]))
     
     if get_dM:
@@ -120,35 +123,40 @@ with pm.Model() as model:
         
     if get_rd:
         #https://arxiv.org/pdf/2106.00428.pdf
-        #wb0 =  0.02236
-        wb0 =  pm.Uniform("wb0", 0.02224, 0.02228)
-        #a1 = 0.00785436
-        #a2 = 0.177084
-        #a3 = 0.00912388
-        #a4 = 0.618711
-        #a5 = 11.9611
-        #a6 = 2.81343
-        #a7 = 0.784719
-        #rd_gp = tt.as_tensor_variable(1/(a1*wb0**a2+a3*wm0**a4+a5*wb0**a6*wm0**a7))  
-        rd_gp = 144.46
+        wb0 =  pm.Uniform("wb0", 0.022, 0.023)
+        a1 = 0.00785436
+        a2 = 0.177084
+        a3 = 0.00912388
+        a4 = 0.618711
+        a5 = 11.9611
+        a6 = 2.81343
+        a7 = 0.784719
+        rd_gp = tt.as_tensor_variable(1/(a1*wb0**a2+a3*wm0**a4+a5*wb0**a6*wm0**a7))  
         
     if get_fs8:
         #s80 = 0.812
         s80 = pm.Normal("s80", 0.8, 0.5)
         Wm0 =  pm.Deterministic('Wm0', wm0*(100/H_gp[0])**2)
         E_gp = pm.Deterministic('E_gp', H_gp/H_gp[0])
+        xx = x_arr[::-1]
+        ee = E_gp[::-1]
+        aa = np.exp(-xx)
         
-        d = tt.zeros(len(z_arr))
-        y = tt.zeros(len(z_arr))
-        d = tt.inc_subtensor(d[-1], a_arr[-1])
-        y = tt.inc_subtensor(y[-1], E_gp[0]*a_arr[-1]**3)
-        for i in np.arange(1, len(z_arr)):
-            A0 = -1.5*Wm0/(a_arr[-i]*E_gp[-i])
-            B0 = -1./(a_arr[-i]**2*E_gp[-i])
-            A1 = -1.5*Wm0/(a_arr[-(i+1)]*E_gp[-(i+1)])
-            B1 = -1./(a_arr[-(i+1)]**2*E_gp[-(i+1)])
-            y = tt.inc_subtensor(y[-(i+1)], (1-0.5*dx**2*A0*B0)*y[-i]-0.5*(A0+A1)*dx*d[-i])
-            d = tt.inc_subtensor(d[-(i+1)], -0.5*(B0+B1)*dx*y[-i]+(1-0.5*dx**2*A0*B0)*d[-i])
+        dd = tt.zeros(len(z_arr))
+        yy = tt.zeros(len(z_arr))
+        dd = tt.inc_subtensor(dd[0], aa[0])
+        yy = tt.inc_subtensor(yy[0], aa[0]**3*E_gp[0])
+        
+        for i in range(len(z_arr)-1):
+            A0 = -1.5*Wm0/(aa[i]*ee[i])
+            B0 = -1./(aa[i]**2*ee[i])
+            A1 = -1.5*Wm0/(aa[i+1]*ee[i+1])
+            B1 = -1./(aa[i+1]**2*ee[i+1])
+            yy = tt.inc_subtensor(yy[i+1], (1+0.5*dx**2*A0*B0)*yy[i]+0.5*(A0+A1)*dx*dd[i])
+            dd = tt.inc_subtensor(dd[i+1], 0.5*(B0+B1)*dx*yy[i]+(1+0.5*dx**2*A0*B0)*dd[i])
+        
+        y = tt.as_tensor_variable(yy[::-1])
+        d = tt.as_tensor_variable(dd[::-1])
         
         fs8_gp = pm.Deterministic('fs8_gp', s80*y/(a_arr**2*E_gp*d[0]))
         s8_gp = pm.Deterministic('s8_gp', s80*d/d[0])
@@ -166,7 +174,7 @@ if 'DESI' in datasets:
         DESI_fs8 = pm.Deterministic('DESI_fs8',
                    tt.as_tensor_variable(fs8_gp[DESI['idx']]+(fs8_gp[DESI['idx']+1]-fs8_gp[DESI['idx']])*DESI['U']))
         theory = tt.concatenate([theory, DESI_H, DESI_dA, DESI_fs8])
-        
+
 if 'H_DESI' in datasets:
     print('Adding H DESI')
     with model:
@@ -271,10 +279,12 @@ if 'FCMB' in datasets:
         FCMB_dM = pm.Deterministic('FCMB_dM',
                   tt.as_tensor_variable(dM_gp[FCMB['idx']]+(dM_gp[FCMB['idx']+1]-dM_gp[FCMB['idx']])*FCMB['U']))
         theory = tt.concatenate([theory, FCMB_dM])
-        
+
+#Sampling
 with model:
     lkl= pm.MvNormal("lkl", mu=theory, cov=data_cov, observed=data)
     trace = pm.sample(n_samples, return_inferencedata=True, tune=n_tune)
+
     
 #print r-stat
 print(pm.summary(trace)['r_hat'][["ℓ","η"]])

scripts/utils.py
@@ -1,149 +1,109 @@
 import numpy as np
-import pymc3 as pm
-import classy
-import theano
-import theano.tensor as tt
-import os
-from scipy.integrate import odeint
-from scipy.interpolate import interp1d
-from scipy.optimize import least_squares
 from pandas import read_table
-import pickle
-
-class utils():
-    def __init__(self):
-        self.c = 299792458.0
     
-    def read_light_curve_parameters(self, path):
-        with open(path, 'r') as text:
-            clean_first_line = text.readline()[1:].strip()
-            names = [e.strip().replace('3rd', 'third')
-                     for e in clean_first_line.split()]
+def read_light_curve_parameters(path):
+    with open(path, 'r') as text:
+        clean_first_line = text.readline()[1:].strip()
+        names = [e.strip().replace('3rd', 'third')
+                 for e in clean_first_line.split()]
 
-        lc_parameters = read_table(
-            path, sep=' ', names=names, header=0, index_col=False)
-        return lc_parameters
-    
-    def get_signal(self, z_arr):
-        ℓ_true = 0.2
-        η_true = 6
-        cov_func = η_true ** 2 * pm.gp.cov.Matern52(1, ℓ_true)
-        mean_func = pm.gp.mean.Zero()
-        return np.random.multivariate_normal(mean_func(z_arr[:, None]).eval(),
-               cov_func(z_arr[:, None]).eval()+1e-8*np.eye(len(z_arr[:, None])),
-               1).flatten()
-    
-    def get_preds(self, z_arr, mode='Planck'):
-        if mode == 'Planck':
-            params = {'h': 0.67,
-              'Omega_cdm': 0.26,
-              'Omega_b': 0.050,
-              'n_s': 0.9665,
-              'ln10^{10}A_s': 3.040}
-        if mode == 'Riess':
-            params = {'h': 0.74,
-              'Omega_cdm': 0.21,
-              'Omega_b': 0.050,
-              'n_s': 0.9665,
-              'ln10^{10}A_s': 3.040}
-        if mode == 'Panth':
-            params = {'h': 0.724,
-              'Omega_cdm': 0.262,
-              'Omega_b': 0.037,
-              'n_s': 0.837,
-              'ln10^{10}A_s': 3.138}
-        self.cosmo = classy.Class()
-        self.cosmo.set({ 'output':'mPk', 'P_k_max_h/Mpc': 20, 'z_max_pk': 1085})
-        self.cosmo.set(params)
-        self.cosmo.compute()
+    lc_parameters = read_table(
+        path, sep=' ', names=names, header=0, index_col=False)
+    return lc_parameters
 
-        self.H0 = self.cosmo.Hubble(0)
-        self.h = self.c*self.H0/100000
-        self.Wm0 = params['Omega_cdm']+params['Omega_b']
-        H_arr = np.array([])
-        dA_arr = np.array([])
-        dL_arr = np.array([])
-        f_arr = np.array([])
-        s8_arr = np.array([])
-        for z in z_arr: 
-            H = self.cosmo.Hubble(z)
-            dA = self.cosmo.angular_distance(z)
-            dL = self.cosmo.luminosity_distance(z)
-            f = self.cosmo.scale_independent_growth_factor_f(z)
-            s8 = self.cosmo.sigma(8./self.cosmo.h(),z)
-            s8_arr = np.append(s8_arr, s8)
-            f_arr = np.append(f_arr, f)
-            dL_arr = np.append(dL_arr, dL)
-            dA_arr = np.append(dA_arr, dA)
-            H_arr = np.append(H_arr, H)
-         
-        dM_arr = dA_arr*(1+z_arr)
-        Hkms_arr = H_arr *self.c/1000
-        preds ={'H_arr': H_arr, 'dA_arr': dA_arr,
-               'dL_arr': dL_arr, 'dM_arr': dM_arr,
-               'Hkms_arr': Hkms_arr, 'f_arr': f_arr, 
-               's8_arr': s8_arr}
-        return preds
-    
-    def make_fs8(self, H, x_arr, wm0, s80, mode='david'):
-        z_arr = np.exp(x_arr)-1
-        a_arr = 1./(1+z_arr) 
-        dx = np.mean(np.diff(x_arr))
-        Wm = wm0*(100/H)**2*(1+z_arr)**3
-        E = H/H[0]
-        
-        if mode=='david':
-            Wm0 = wm0*(100/H[0])**2
-            d = np.zeros(len(z_arr))
-            y = np.zeros(len(z_arr))
-            d[-1] = a_arr[-1]
-            y[-1] = E[0]*a_arr[-1]**3
+def get_signal(z_arr):
+    ℓ_true = 0.2
+    η_true = 6
+    cov_func = η_true ** 2 * pm.gp.cov.Matern52(1, ℓ_true)
+    mean_func = pm.gp.mean.Zero()
+    return np.random.multivariate_normal(mean_func(z_arr[:, None]).eval(),
+           cov_func(z_arr[:, None]).eval()+1e-8*np.eye(len(z_arr[:, None])),
+           1).flatten()
 
-            for i in np.arange(1, len(z_arr)):
-                A0 = -1.5*Wm0/(a_arr[-i]*E[-i])
-                B0 = -1./(a_arr[-i]**2*E[-i])
-                A1 = -1.5*Wm0/(a_arr[-(i+1)]*E[-(i+1)])
-                B1 = -1./(a_arr[-(i+1)]**2*E[-(i+1)])
-                y[-(i+1)] = (1-0.5*dx**2*A0*B0)*y[-i]-0.5*(A0+A1)*dx*d[-i]
-                d[-(i+1)] = -0.5*(B0+B1)*dx*y[-i]+(1-0.5*dx**2*A0*B0)*d[-i]
+def get_preds(z_arr, cosmo):
+    c = 299792458.0
+    H0 = cosmo.Hubble(0)
+    h = H0/100000
+    Wm0 = cosmo.Omega_m()
+    H_arr = np.array([])
+    dA_arr = np.array([])
+    dL_arr = np.array([])
+    f_arr = np.array([])
+    s8_arr = np.array([])
+    for z in z_arr: 
+        H = cosmo.Hubble(z)
+        dA = cosmo.angular_distance(z)
+        dL = cosmo.luminosity_distance(z)
+        f = cosmo.scale_independent_growth_factor_f(z)
+        s8 = cosmo.sigma(8./cosmo.h(),z)
+        s8_arr = np.append(s8_arr, s8)
+        f_arr = np.append(f_arr, f)
+        dL_arr = np.append(dL_arr, dL)
+        dA_arr = np.append(dA_arr, dA)
+        H_arr = np.append(H_arr, H)
 
-            fs8 = s80*y/(a_arr**2*E*d[0])
-            s8 = s80*d/d[0]
-        
-        if mode=='jaime':
-            
-            comf_H = a_arr*H
-            diff_comf_H = np.zeros(len(z_arr))
-            diff_comf_H[0] = (comf_H[1]-comf_H[0])/(x_arr[1]-x_arr[0])
-            diff_comf_H[1:-1] = (comf_H[2:]-comf_H[:-2])/(x_arr[2:]-x_arr[:-2])
-            diff_comf_H[-1] = (comf_H[-1]-comf_H[-2])/(x_arr[-1]-x_arr[-2])
-            q = 1-(diff_comf_H/comf_H)
+    dM_arr = dA_arr*(1+z_arr)
+    Hkms_arr = H_arr*c/1000
+    preds ={'H_arr': H_arr, 'dA_arr': dA_arr,
+           'dL_arr': dL_arr, 'dM_arr': dM_arr,
+           'Hkms_arr': Hkms_arr, 'f_arr': f_arr, 
+           's8_arr': s8_arr, 'fs8_arr': s8_arr*f_arr}
+    return preds
 
-            #Implement second Order Runge-Kutta method
-            f = np.zeros(len(z_arr))
-            f[-1] = 1
-            for i in np.arange(1, len(z_arr)):
-                k0 = -1*((3/2)*Wm[-i]-f[-i]**2-q[-i]*f[-i])
-                f1 = f[-i]-dx*k0
-                k1 = -1*((3/2)*Wm[-(i+1)]-f1**2-q[-(i+1)]*f1)
-                f[-(i+1)] = f[-i]-dx*(k1+k0)/2
+def make_fs8(H, x_arr, wm0, s80):
+    z_arr = np.exp(x_arr)-1
+    a_arr = 1./(1+z_arr) 
+    dx = np.mean(np.diff(x_arr))
+    Wm = wm0*(100/H)**2*(1+z_arr)**3
+    E = H/H[0]
+    Om = wm0*(100/H[0])**2
+    d = np.zeros(len(z_arr))
+    y = np.zeros(len(z_arr))
+    d[-1] = a_arr[-1]
+    y[-1] = E[0]*a_arr[-1]**3
 
-            #integrate for s8 method2
-            s8 = np.zeros(len(z_arr))
-            s8[0] = s80
-            for i in np.arange(1, len(z_arr)):
-                k0 = -1*f[i-1]*s8[i-1]
-                s8[i] = s8[i-1] + dx*(k0)
+    xx = x_arr[::-1]
+    ee = E[::-1]
+    aa = np.exp(-xx)
+    dx = np.mean(np.diff(xx))
 
-            fs8 = f*s8
-        
-        return s8, fs8
-    
-    def make_dM(self, H, x_arr):
-        z_arr = np.exp(x_arr)-1
-        a_arr = 1./(1+z_arr) 
-        dx = np.mean(np.diff(x_arr))
-        dM = np.zeros(len(z_arr)+1)
-        dM[1:] = dx*np.cumsum((1+z_arr)/H)
-        dM = 0.5*(dM[1:]+dM[:-1])-0.5*dM[1]
-        return dM
+    nz = len(aa)
+    dd = np.zeros(nz)
+    yy = np.zeros(nz)
+    dd[0] = aa[0]
+    yy[0] = aa[0]**3*E[0]
+
+    for i in range(nz-1):
+        A0 = -1.5*Om/(aa[i]*ee[i])
+        B0 = -1./(aa[i]**2*ee[i])
+        A1 = -1.5*Om/(aa[i+1]*ee[i+1])
+        B1 = -1./(aa[i+1]**2*ee[i+1])
+        yy[i+1]=(1+0.5*dx**2*A0*B0)*yy[i]+0.5*(A0+A1)*dx*dd[i]
+        dd[i+1]=0.5*(B0+B1)*dx*yy[i]+(1+0.5*dx**2*A0*B0)*dd[i]
+
+    y = yy[::-1]
+    d = dd[::-1]
+    fs8 = s80*y/(a_arr**2*E*d[0])
+    s8 = s80*d/d[0]
+
+    return s8, fs8
+
+def make_dM(H, x_arr):
+    z_arr = np.exp(x_arr)-1
+    a_arr = 1./(1+z_arr) 
+    dx = np.mean(np.diff(x_arr))
+    dM = np.zeros(len(z_arr)+1)
+    dM[1:] = dx*np.cumsum((1+z_arr)/H)
+    dM = 0.5*(dM[1:]+dM[:-1])-0.5*dM[1]
+    return dM
+
+def make_rd(wm0, wb0):
+    a1 = 0.00785436
+    a2 = 0.177084
+    a3 = 0.00912388
+    a4 = 0.618711
+    a5 = 11.9611
+    a6 = 2.81343
+    a7 = 0.784719
+    rd_gp = 1/(a1*wb0**a2+a3*wm0**a4+a5*(wb0**a6)*(wm0**a7))
+    return rd_gp